35693fbb699b8e555dfb0caaa06283d5d091d1d1
Elena 879 <elena.zabolotnaya18@gmail.com>
2017-12-22 18:06:09 +0200

    popup fix

--- source-js/main.js
+++ source-js/main.js
@@ -1,19 +1,19 @@
-/*jQuery(window).load(function () {
+jQuery(window).load(function () {
   jQuery('.flexslider').flexslider({
     animation: 'slide',
     controlNav: false,
     directionNav: false
   })
-})*/
-
+})
 
 jQuery(document).ready(function (event) {
   jQuery('.learn-more-button').on('click', function () {
-    jQuery('.popup').addClass('show')
+    jQuery('.popup').fadeIn(300)
   })
 })
+
 jQuery(document).ready(function (event) {
   jQuery('.close-popup').on('click', function () {
-    jQuery('.popup').removeClass('show')
+    jQuery('.popup').fadeOut(200)
   })
 })
