abe8a0193e4427648c2e1710175f97b87bf06fd8
Elena 879 <elena.zabolotnaya18@gmail.com>
2017-12-24 01:08:01 +0200

    other sliders

--- source-js/main.js
+++ source-js/main.js
@@ -2,7 +2,31 @@ jQuery(window).load(function () {
   jQuery('.flexslider').flexslider({
     animation: 'slide',
     controlNav: false,
-    directionNav: false
+    directionNav: false,
+    slideshowSpeed : 5000
+  })
+
+  jQuery('.flexslider-introducing').flexslider({
+    animation: "slide",
+    animationLoop: false,
+    itemWidth: 380,
+    itemMargin: 30,
+    controlNav: false,
+    customDirectionNav:".go-over"
+  })
+
+  jQuery('.flexslider-videos').flexslider({
+    animation: "slide",
+    animationLoop: false,
+    controlNav: false,
+    customDirectionNav:".go-over"
+  })
+
+  jQuery('.flexslider-concert').flexslider({
+    animation: 'slide',
+    controlNav: false,
+    directionNav: false,
+    slideshowSpeed : 8000
   })
 })
 
@@ -10,9 +34,7 @@ jQuery(document).ready(function (event) {
   jQuery('.learn-more-button').on('click', function () {
     jQuery('.popup').fadeIn(300)
   })
-})
 
-jQuery(document).ready(function (event) {
   jQuery('.close-popup').on('click', function () {
     jQuery('.popup').fadeOut(200)
   })
